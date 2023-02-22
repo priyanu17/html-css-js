9dcbcd55eac0b89a6d5a2f07880487a1e01c32b7
priyanu17 <anu396priya@gmail.com>
2023-02-22 16:49:28 +0530

    refactor code

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,12 +1,15 @@
-'use-strict';
+'use strict';
 
 const modal = document.querySelector('.modal');
 
 const openButtons = document.querySelectorAll('.btn-open-modal');
 
+const overlay = document.querySelector('.overlay');
+
 function openModalWindow() {
   console.log('button clicked');
   modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }
 openButtons.forEach((openButton) => {
   openButton.addEventListener('click', openModalWindow);
@@ -16,6 +19,21 @@ const closeModalButtons = document.querySelector('.close-modal');
 function closeModalWindow() {
   console.log('close button clicked');
   modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 }
 
 closeModalButtons.addEventListener('click', closeModalWindow);
+
+overlay.addEventListener('click', closeModalWindow);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+
+  if (
+    e.key === 'Escape' &&
+    !modal.classList.contains('hidden') &&
+    !overlay.classList.contains('hidden')
+  ) {
+    closeModalWindow();
+  }
+});
