f77dd0e2e57fb726445a73dbd4942528cd6742ae
priyanu17 <anu396priya@gmail.com>
2023-03-02 11:10:20 +0530

    switch player

--- pig-game/script.js
+++ pig-game/script.js
@@ -5,12 +5,14 @@ const playerDiceScoresEle = document.querySelectorAll('.player-dice-score');
 const playerOneTitleEle = document.getElementById('player-title-one');
 const playerTwoTitleEle = document.getElementById('player-title-two');
 const scoreTitlesEle = document.querySelectorAll('.score-title');
-const playerScoreOneEle = document.getElementById('player-score-one');
-const playerScoreTwoEle = document.getElementById('player-score-two');
+const playerScoreOneEle = document.getElementById('player-score-1');
+const playerScoreTwoEle = document.getElementById('player-score-2');
 const diceImageEle = document.querySelector('.dice-image');
-const rollTheDiceButton = document.querySelector('.btn btn-roll-dice');
-const gameSectionOne = document.getElementById('game-section-one');
-const gameSectionTwo = document.getElementById('game-section-two');
+const rollTheDiceButton = document.querySelector('.btn-roll-dice');
+const newGameButton = document.querySelector('.btn-new-game');
+const holdButton = document.querySelector('.btn-hold');
+const gameSectionOne = document.getElementById('game-section-1');
+const gameSectionTwo = document.getElementById('game-section-2');
 
 // setting player scores to 0
 playerDiceScoresEle.forEach((scoreElement) => {
@@ -30,5 +32,40 @@ scoreTitlesEle.forEach((scoreTitle) => {
 playerScoreOneEle.textContent = '0';
 playerScoreTwoEle.textContent = '0';
 
+//make player one as active
 // hide dice image on load
 diceImageEle.classList.add('hidden');
+gameSectionOne.classList.add('active');
+
+// current score variable
+
+let activePlayer = 0;
+let currentScore = 0;
+let playerScores = [0, 0];
+
+rollTheDiceButton.addEventListener('click', function () {
+  // roll the dice, make current player as active
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //display the dice
+  diceImageEle.src = `./imgs/dice-${dice}.png`;
+  diceImageEle.classList.remove('hidden');
+
+  //check for one
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`player-score-${activePlayer + 1}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`player-score-${activePlayer + 1}`).textContent = 0;
+    document
+      .getElementById(`game-section-${activePlayer + 1}`)
+      .classList.remove('active');
+
+    activePlayer = activePlayer === 1 ? 0 : 1;
+    currentScore = 0;
+    document
+      .getElementById(`game-section-${activePlayer + 1}`)
+      .classList.add('active');
+  }
+});
